06913c994b658f64e3b48fb7e644cf27534f1e06
hx3ndryy <archiar2010@gmail.com>
2026-05-17 21:34:02 +0300

    Update cache paths to portfoliomaket1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='mangal-haus-v1';
-const URLS=['/mangal-haus/','/mangal-haus/index.html','/mangal-haus/css/style.css','/mangal-haus/js/script.js','/mangal-haus/manifest.json'];
+const URLS=['/portfoliomaket1/','/portfoliomaket1/index.html','/portfoliomaket1/css/style.css','/portfoliomaket1/js/script.js','/portfoliomaket1/manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(i=>i!==CACHE).map(i=>caches.delete(i)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
